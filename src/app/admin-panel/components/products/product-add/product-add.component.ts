4519cd39734247d25729da1b282862463906bcd5
ashishav7 <ashish.vyas81@gmail.com>
2025-01-14 22:37:38 +0530

    Error messages added

--- src/app/admin-panel/components/products/product-add/product-add.component.ts
+++ src/app/admin-panel/components/products/product-add/product-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ConstantsAdminService } from 'src/app/services/admin/constants-admin.service';
 import { ConstantsService } from 'src/app/services/constants.service';
+import { ErrormessagesService } from 'src/app/services/errormessages.service';
 
 @Component({
   selector: 'app-product-add',
@@ -13,10 +14,13 @@ export class ProductAddComponent implements OnInit{
   floatLabel : any = 'auto';
   categories:any;
   labels:any={};
-  constructor(fb:FormBuilder,labels:ConstantsService){
+  isSubmitted:boolean=false;
+  errorMessages:any={};
+  constructor(fb:FormBuilder,labels:ConstantsService,errorNessages:ErrormessagesService){
     this.labels=labels.labelMessages;
+    this.errorMessages=errorNessages.errorMessages;
     this.productForm = fb.group({
-      name : ['',Validators.required],
+      name : ['',[Validators.required]],
       description : ['', Validators.required],
       category : ['', Validators.required],
       quantity : ['',Validators.required],
@@ -42,4 +46,58 @@ export class ProductAddComponent implements OnInit{
       categoryId:1,categoryName:'Electronics' 
     }];
   }
+
+  onSubmit(){
+    this.isSubmitted=true;
+    console.log(this.productForm);
+    if(!this.productForm.valid){
+      console.log(this.productForm.get('name'));
+      return;
+    }
+  }
+
+  getNameErrorMessage(){
+    if(this.productForm.get('name')?.hasError("required")){
+      return this.errorMessages.productNameRequired;
+    }else if(this.productForm.get('name')?.hasError("minlength")){
+      return "Min 3 length";
+    }
+  }
+  getCategoryErrorMessage(){
+    if(this.productForm.get('category')?.hasError("required")){
+      return this.errorMessages.productCategoryRequired;
+    }
+  }
+
+  getDescriptionErrorMessage(){
+    if(this.productForm.get('description')?.hasError("required")){
+      return this.errorMessages.productDescriptionRequired;
+    }
+  }
+
+  getQuantityErrorMessage(){
+    if(this.productForm.get('quantity')?.hasError("required")){
+      return this.errorMessages.productQuanityRequired;
+    }
+  }
+
+  getSellingPriceErrorMessage(){
+    if(this.productForm.get('sellingPrice')?.hasError("required")){
+      return this.errorMessages.productSellingPriceRequired;
+    }
+  }
+
+  getCostPriceErrorMessage(){
+    if(this.productForm.get('costPrice')?.hasError("required")){
+      return this.errorMessages.productCostPriceRequired;
+    }
+  }
+
+  getImageErrorMessage(){
+    if(this.productForm.get('image')?.hasError("required")){
+      return this.errorMessages.productImageRequired;
+    }
+  }
+
+
 }
---
+++ src/app/services/errormessages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrormessagesService {
+
+  constructor() { }
+  readonly errorMessages={
+    required : "is required",
+    productNameRequired : `Name is required`,
+    productDescriptionRequired : "Description is required",
+    productCategoryRequired : "Category is required",
+    productQuantityRequired : "Quantity is required",
+    productCostPriceRequired : "Cost Price is required",
+    productSellingPriceRequired : "Selling Price is required",
+    productImageRequired : "Product Image is required"
+  }
+}
