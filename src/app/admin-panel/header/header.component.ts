c467cd7432b4b628d2c1a41d2f92d88241e6931c
ashishav7 <ashish.vyas81@gmail.com>
2025-01-18 18:36:30 +0530

    side bar routing done

--- src/app/admin-panel/header/header.component.ts
+++ src/app/admin-panel/header/header.component.ts
@@ -11,23 +11,11 @@ import { ConstantsService } from 'src/app/services/constants.service';
 export class HeaderComponent implements OnInit {
   labels: any = {};
   labelsAdmin: any = {};
-  router:Router;
   constructor(labels: ConstantsService, labelsAdmin: ConstantsAdminService,router:Router) {
-    this.labels = labels.labelMessages;
-    this.labelsAdmin = labelsAdmin.labelMessages;
-    this.router=router;
   }
 
   ngOnInit(): void {
     console.log(this.labels);
   }
-  goToMenuHeader1() {
-    this.router.navigate(['/admin-panel/products']);
-  }
-  goToMenuHeader2() {
-    this.router.navigate(['/admin-panel']);
-  }
-  goToMenuHeader3() {
-    this.router.navigate(['/admin-panel/orders']);
-  }
+ 
 }
--- src/app/shared/component/sidebar/sidebar.component.ts
+++ src/app/shared/component/sidebar/sidebar.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConstantsAdminService } from 'src/app/services/admin/constants-admin.service';
 import { ApiService } from 'src/app/services/api.service';
 import { ConstantsService } from 'src/app/services/constants.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent implements OnInit{
-  labels:any={}
-  categories:any={};
-  constructor(public label:ConstantsService,private api:ApiService){
-    this.labels=label.labelMessages;
+export class SidebarComponent implements OnInit {
+  labels: any = {};
+  categories: any = {};
+  labelsAdmin: any = {};
+  constructor(
+    labels: ConstantsService,
+    labelsAdmin: ConstantsAdminService,
+    public router: Router,
+    private api: ApiService
+  ) {
+    this.labels = labels.labelMessages;
+    this.labelsAdmin = labelsAdmin.labelMessages;
+  }
+  ngOnInit(): void {
+    this.categories = this.api.getCategories();
   }
 
-  ngOnInit(): void {
-    this.categories=this.api.getCategories();
+  goToMenuHeader1() {
+    this.router.navigate(['/admin-panel/products']);
+  }
+  goToMenuHeader2() {
+    this.router.navigate(['/admin-panel']);
+  }
+  goToMenuHeader3() {
+    this.router.navigate(['/admin-panel/orders']);
   }
 }
